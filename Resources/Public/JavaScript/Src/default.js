e2d2097650df456bd4fefa3ff70c243ad4c3c578
AKaufhold <info@stackfactory.de>
2021-06-24 16:30:41 +0200

    [ADD] import jquery-ui

--- Resources/Public/JavaScript/Src/default.js
+++ Resources/Public/JavaScript/Src/default.js
@@ -1,6 +1,15 @@
+require('jquery-ui');
 import $ from 'jquery';
 import 'bootstrap';
 
 $(function(){ 
-	
+	console.log("test");
+    alert("test"); 
+
+    $('.tooltip').tooltip({
+        sanitizeFn: function (content) {
+          return DOMPurify.sanitize(content)
+        }
+    });
+
 });
