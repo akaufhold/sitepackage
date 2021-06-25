4cf4569d58bbfa0963f0bc539e5b836709dd101b
AKaufhold <info@stackfactory.de>
2021-06-25 18:33:07 +0200

    [Add] Empty Custom Content Elements

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,3 +10,11 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sitepackage'] = 'EXT:sitepackage/
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:sitepackage/Configuration/TsConfig/Page/All.tsconfig">');
+
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+   'custom-content',
+   \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
+   // font-awesome identifier ('external-link-square')
+   ['name' => 'external-link-square']
+);
